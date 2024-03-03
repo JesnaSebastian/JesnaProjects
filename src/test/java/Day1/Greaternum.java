6d1c726c2d9797407c5b1dd8a827e29a445b64d1
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2024-03-03 22:43:14 +0530

    first commit

---
+++ src/test/java/Day1/Greaternum.java
@@ -0,0 +1,23 @@
+package Day1;
+
+public class Greaternum {
+    public static void main(String[] args) {
+
+        int a=10,b=78,c=8;
+
+        if(a>b && a>c)
+        {
+           System.out.println("a is the greater number "+a) ;
+        }
+        else if (b>c)
+        {
+            System.out.println("b is the greater number "+b) ;
+        }
+        else
+        {
+            System.out.println("c is the greater number "+c) ;
+        }
+
+    }
+
+}
