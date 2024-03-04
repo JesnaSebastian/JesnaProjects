87aa5e0465a945ea2b140d688d3c49453a903771
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2024-03-04 08:06:40 +0530

    Second

--- src/test/java/Day1/Swaptwonumbers.java
+++ src/test/java/Day1/Swaptwonumbers.java
@@ -1,4 +1,14 @@
 package Day1;
 
 public class Swaptwonumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 15;
+        System.out.println("Before swapping");
+        System.out.println("a value is " + a +" "+"and"+" "+ "b value is  " + b);
+        System.out.println("After swapping");
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a value is " + a +" "+"and"+" "+ "b value is  " + b);
+    }
 }
