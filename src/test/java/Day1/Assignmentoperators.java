0c2ba2feb640134c7eee70ac37995e96a4cfef96
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2024-03-04 10:17:44 +0530

    all

---
+++ src/test/java/Day1/Assignmentoperators.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class Assignmentoperators {
+    public static void main(String[] args) {
+        int a=80;
+        a+=10;
+        System.out.println("value of a after addition "+a);
+        a-=10;
+        System.out.println("value of a after subtraction "+a);
+        a*=2;
+        System.out.println("value of a after multiplication "+a);
+        a/=2;
+        System.out.println("value of a after division "+a);
+        a%=2;
+        System.out.println("mod a is "+a);
+    }
+}
+
---
+++ src/test/java/Day1/Unaryoperators.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Unaryoperators {
+    public static void main(String[] args) {
+        int a=10;
+        int b=++a;
+        System.out.println(a);
+        System.out.println(b);
+        int c=a++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
