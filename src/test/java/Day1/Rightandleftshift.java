8e7de1c18aa182bf6be22819b30b2b68bd509ccd
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2024-03-04 08:23:03 +0530

    shiftoperators

---
+++ src/test/java/Day1/Rightandleftshift.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class Rightandleftshift {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a << 2;
+        int c = a >> 2;
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+
+
